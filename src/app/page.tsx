35a1a445ab7de186e7875ea6ae7009c3ce41ea22
Julian Archila <julianarchilacaro@gmail.com>
2024-07-06 09:15:06 -0500

    remove main page slow components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-import SlowComponent from "@/components/slow";
-import { Suspense } from "react";
+// import SlowComponent from "@/components/slow";
+// import { Suspense } from "react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Suspense fallback={<div>Loading...</div>}>
+      {/* <Suspense fallback={<div>Loading...</div>}>
         <SlowComponent />
-      </Suspense>
+      </Suspense> */}
+      <Link href="/chat">
+        <a className="text-2xl font-bold text-blue-500 hover:text-blue-700">
+          Chat
+        </a>
+      </Link>
     </main>
   );
 }
